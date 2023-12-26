d4cb8a6301e449be727709ab1708f31d8abf22d1
Patryk <pgb972@gmail.com>
2023-12-26 13:35:08 +0000

    initial app setup

---
+++ FF14-Wordle/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FF14_Wordle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
